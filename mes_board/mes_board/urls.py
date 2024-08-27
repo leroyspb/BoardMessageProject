f9d9048e7029321bdffa8b7c74a60a58b2caa958
leroyspb <leroy777@bk.ru>
2024-08-27 06:12:26 +0300

    27.08 inception

--- mes_board/mes_board/urls.py
+++ mes_board/mes_board/urls.py
@@ -19,9 +19,10 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('', include('board.urls')),
     path('admin/', admin.site.urls),
-    path("accounts", include("django.contrib.auth.urls")),
+    path("accounts/", include("allauth.urls")),
+    path("accounts/", include("accounts.urls")),
+    path('', include('board.urls')),
 
 ]
 
